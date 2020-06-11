99331a3091550d88023e8c1124fa36956e810250
Chemil <chemil.makhlouta@gmail.com>
2020-06-11 01:06:57 -0700

    Sets up Router for use
    Uses router to display different pages on click within the navigation bar

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Link} from 'react-router-dom';
 
 const navLinks = [
     {
@@ -26,8 +27,7 @@ export default function Navigation() {
         <ul>
             { navLinks.map((link, index) => (
                 <li key={index}>
-                    {link.title}
-            
+                    <Link to={link.path}>{link.title}</Link>
                 </li>
             ))}
         </ul>
