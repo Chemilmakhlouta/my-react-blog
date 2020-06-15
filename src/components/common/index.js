819f7d8dc73c1e300d165789a3d4373806fe41dd
Chemil <chemil.makhlouta@gmail.com>
2020-06-14 22:51:23 -0700

    Adds Recent posts
    Adds Recent Posts section with pagination to multiple pages of posts.
    
    Fixes mobile view menu going under images

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -1,7 +1,11 @@
 import MasonryPost from './masonry-post'
 import PostMasonry from './post-masonry'
+import PostGrid from './post-grid'
+import TagRow from './tag-row'
 
 export {
     MasonryPost,
-    PostMasonry
+    PostMasonry,
+    PostGrid,
+    TagRow
 }
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {PostMasonry, MasonryPost} from '../components/common'
+import {PostMasonry, MasonryPost, PostGrid} from '../components/common'
 import trending from '../assets/mocks/trending'
 import featured from '../assets/mocks/featured'
 
@@ -30,8 +30,12 @@ const featuredConfig = {
 const mergeStyles = function (posts, config) {
     posts.forEach((post,index) => {
         post.style = config[index]
+        post.author = "Chemil Coder"
+        post.description = "Lorem ipsum dolor sit amet, consectetur"
     });
 }
+
+const recentPosts = [...trending, ...featured, ...featured]
 
 mergeStyles(trending, trendingConfig)
 mergeStyles(featured, featuredConfig)
@@ -40,18 +44,30 @@ const lastFeaturedPost = featured.pop()
 
 export default function Home () {
     return (
-        <section className="container home">
-            <div className="row">
-                <h1>Featured Posts</h1>
-                <section className="featured-posts-container">
-                    <PostMasonry posts={featured} columns={2} tagsOnTop={true}/>
-                    <MasonryPost post={lastFeaturedPost} tagsOnTop={true}/>
+        <main className="home">
+            <section className="container">
+                <div className="row">
+                    <section className="featured-posts-container">
+                        <PostMasonry posts={featured} columns={2} tagsOnTop={true}/>
+                        <MasonryPost post={lastFeaturedPost} tagsOnTop={true}/>
+                    </section>
+                </div>
+            </section>
+
+            <section className="bg-white">
+                <section className="container">
+                    <div className="row">
+                        <h1>Recent Posts</h1>
+                        <PostGrid posts={recentPosts}/>
+                    </div>
                 </section>
+            </section>
 
-
-                <h1>Trending Posts</h1>
-                <PostMasonry posts={trending} columns={3}/>
-            </div>
-        </section>
+            <section className="container">
+                <div className="row">
+                    <PostMasonry posts={trending} columns={3}/>
+                </div>
+            </section>
+        </main>
     )
 }
